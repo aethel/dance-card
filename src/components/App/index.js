3f79f7010987993e7a66952b249429334f732006
aethel <aethelbrihte@gmail.com>
2019-02-24 12:58:27 +0100

    session handling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
@@ -11,65 +11,28 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
-import { withFirebase } from '../Firebase';
-import { AuthUserContext } from '../Session';
+import { withAuthentication } from '../Session';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
 
-        this.state = {
-            authUser: null,
-        };
-    }
+      <hr />
 
-    componentDidMount() {
-        this.listener = this.props.firebase.auth.onAuthStateChanged(
-            authUser => {
-                authUser
-                    ? this.setState({ authUser })
-                    : this.setState({ authUser: null });
-            },
-        );
-    }
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route
+        exact
+        path={ROUTES.PASSWORD_FORGET}
+        component={PasswordForgetPage}
+      />
+      <Route exact path={ROUTES.HOME} component={HomePage} />
+      <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
+);
 
-    componentWillUnmount() {
-        this.listener();
-    }
-
-    render() {
-        return (
-            <AuthUserContext.Provider value={this.state.authUser}>
-                <Router>
-                    <div>
-                        <Navigation />
-
-                        <hr />
-
-                        <Route
-                            exact
-                            path={ROUTES.LANDING}
-                            component={LandingPage}
-                        />
-                        <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-                        <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-                        <Route
-                            exact
-                            path={ROUTES.PASSWORD_FORGET}
-                            component={PasswordForgetPage}
-                        />
-                        <Route exact path={ROUTES.HOME} component={HomePage} />
-                        <Route
-                            exact
-                            path={ROUTES.ACCOUNT}
-                            component={AccountPage}
-                        />
-                        <Route exact path={ROUTES.ADMIN} component={AdminPage} />
-                    </div>
-                </Router>
-            </AuthUserContext.Provider>
-        );
-    }
-}
-
-export default withFirebase(App);
+export default withAuthentication(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,37 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
+
 import SignOutButton from '../SignOut';
+import * as ROUTES from '../../constants/routes';
+
 import { AuthUserContext } from '../Session';
 
-const Navigation = ({ authUser }) => (
-    <div>
-        <AuthUserContext.Consumer>
-            {authUser ? <NavigationAuth /> : <NavigationNonAuth />}
-        </AuthUserContext.Consumer>
-    </div>
-)
+const Navigation = () => (
+  <div>
+    <AuthUserContext.Consumer>
+      {authUser =>
+        authUser ? <NavigationAuth /> : <NavigationNonAuth />
+      }
+    </AuthUserContext.Consumer>
+  </div>
+);
 
 const NavigationAuth = () => (
-    <div>
-        <ul>
-            <li><Link to={ROUTES.LANDING}>Landing</Link></li>
-            <li><Link to={ROUTES.HOME}>Home</Link></li>
-            <li><Link to={ROUTES.ACCOUNT}>Account</Link></li>
-            <li><SignOutButton /></li>
-        </ul>
-    </div>
-)
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
 const NavigationNonAuth = () => (
-    <div>
-        <ul>
-            <li><Link to={ROUTES.LANDING}>Landing</Link></li>
-            <li>
-                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-            </li>
-            <li><Link to={ROUTES.ADMIN}>Admin</Link></li>
-        </ul>
-    </div>
-)
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
+);
 
 export default Navigation;
