2342786b3309d5ba9bbd1b016f710a604271c59b
aethel <aethelbrihte@gmail.com>
2019-04-16 16:39:26 +0200

    Missing marker fix

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 const PopupMarker = ({ children, position }) => (
     <Marker position={position}>
         <Popup>
