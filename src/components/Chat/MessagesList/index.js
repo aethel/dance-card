b0f71afdf349f611595ebaeeacc1aefba487a330
aethel <aethelbrihte@gmail.com>
2019-08-31 19:24:09 +0200

    messages lsit

--- src/components/Chat/MessagesList/index.js
+++ src/components/Chat/MessagesList/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { withFirebase } from '../../Firebase';
 import { map, flatMap, takeLast } from 'rxjs/operators';
 import { combineLatest } from 'rxjs';
@@ -7,49 +7,57 @@ import { collection } from 'rxfire/firestore';
 const uid = sessionStorage.getItem('uid');
 
 class MessageListBase extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            messages: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+  }
 
-    componentDidMount() {
-        const fromRef = this.props.firebase.chats().where('fromID', '==', uid);
-        const toRef = this.props.firebase.chats().where('toID', '==', uid);
-        const fromRef$ = collection(fromRef);
-        const toRef$ = collection(toRef);
+  componentDidMount() {
+    const fromRef = this.props.firebase.chats().where('fromID', '==', uid);
+    const toRef = this.props.firebase.chats().where('toID', '==', uid);
+    const fromRef$ = collection(fromRef);
+    const toRef$ = collection(toRef);
 
-        const messages$ = combineLatest(fromRef$, toRef$).pipe(
-            flatMap(msgs => {
-                const [incoming, sent] = msgs;
-                return [...incoming, ...sent]
-            }),
-            map(docs => docs.data())
-        )
-        messages$.subscribe((item, index) => {
-            const messages = [...this.state.messages, item];
-            this.setState({ messages })
-        })
-    }
+    const messages$ = combineLatest(fromRef$, toRef$).pipe(
+      flatMap(msgs => {
+        const [incoming, sent] = msgs;
+        return [...incoming, ...sent];
+      }),
+      map(docs => docs.data())
+    );
+    messages$.subscribe((item, index) => {
+      const messages = [...this.state.messages, item];
+      this.setState({ messages });
+    });
+  }
 
+  render() {
+    const { messages } = this.state;
+    console.log(messages);
 
-
-    render() {
-        const { messages } = this.state;
-        console.log(messages);
-        
-        return (
-            <ul>
-                {messages.sort((item1, item2) => item1.timestamp > item2.timestamp ).map((item, index) => (
-                    <li key={`${item.message}${index}`}> 
-                        <time>{new Date(item.timestamp).toLocaleDateString('en-GB', { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })}</time>
-                        <p>{item.message}</p>
-                    </li>
-                ))}
-            </ul>
-        )
-    }
+    return (
+      <ul>
+        {/* {messages.sort((item1, item2) => item1.timestamp > item2.timestamp ).map((item, index) => ( */}
+        {this.props.messages
+          .sort((item1, item2) => item1.timestamp > item2.timestamp)
+          .map((item, index) => (
+            <li key={`${item.message}${index}`}>
+              <time>
+                {new Date(item.timestamp).toLocaleDateString('en-GB', {
+                  month: 'long',
+                  day: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit'
+                })}
+              </time>
+              <p>{item.message}</p>
+            </li>
+          ))}
+      </ul>
+    );
+  }
 }
 
 const MessagesList = withFirebase(MessageListBase);
--- src/components/Chats/index.js
+++ src/components/Chats/index.js
@@ -1,75 +1,106 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { AuthUserContext } from '../Session';
 import { withFirebase } from '../Firebase';
-import { ProfileProvider, useProfile, ProfileConsumer } from '../Account/ProfileProvider/profile-provider';
+import {
+  ProfileProvider,
+  useProfile,
+  ProfileConsumer
+} from '../Account/ProfileProvider/profile-provider';
+import MessagesList from '../Chat/MessagesList';
 import { flatMap } from 'rxjs/operators';
 import { combineLatest } from 'rxjs';
 import { collectionData } from 'rxfire/firestore';
 
 const ChatsPage = () => (
-    <div>
-        <AuthUserContext.Consumer>{authUser => authUser && (
-            <ProfileProvider>
-                <ProfileConsumer>{profile =>
-                    <ChatsComponent profile={profile} />}
-                </ProfileConsumer>
-            </ProfileProvider>
-        )}
-        </AuthUserContext.Consumer>
-    </div>
+  <div>
+    <AuthUserContext.Consumer>
+      {authUser =>
+        authUser && (
+          <ProfileProvider>
+            <ProfileConsumer>
+              {profile => <ChatsComponent profile={profile} />}
+            </ProfileConsumer>
+          </ProfileProvider>
+        )
+      }
+    </AuthUserContext.Consumer>
+  </div>
 );
 
-// const groupBy = key => array => array.reduce((objectsByKeyValue, obj) => {
-// const value = obj[key];
-// objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
-// return objectsByKeyValue;
-// },{});
-// const groupByFromUser = groupBy('fromUser');
+const groupBy = key => array =>
+  array.reduce((objectsByKeyValue, obj) => {
+    const value = obj[key];
+    objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+    return objectsByKeyValue;
+  }, []);
 
-const ChatsBase = (props) => {
-    const [state, setState] = useState([]);
-    const { profile } = useProfile();
-    useEffect(() => {
-        console.log('loaded first time', profile.id);
-        if (profile.id) {
-            const chatIDs = [];
-            profile.chats.forEach(chatID =>
-                 chatIDs.push(props.firebase.chats().where('chatID', '==', chatID).get()))
-            Promise.all(chatIDs).then(results => {
-                const mappedResult = results.map(result => result.docs.map(doc => doc.data()))
-                console.log(mappedResult);
-                
-                setState(mappedResult.flat());
-                }
-            )};
+const ChatsBase = props => {
+  const [state, setState] = useState([]);
+  const { profile } = useProfile();
+  console.log(profile);
 
-            // const messages$ = combineLatest(fromRef$, toRef$).pipe(
-            //     flatMap(msgs => {
-            //         const [incoming, sent] = msgs;
-            //         return [...incoming, ...sent]
-            //     })
-            // )
-            // messages$.subscribe((item) => {
-            //     const messages = [...state, item];
-            //     setState(messages)
-            // })
-            console.log(state);
-            
-    }, [profile.id])
+  useEffect(() => {
+    console.log('loaded first time', profile.id);
+    if (profile.id) {
+      const chatIDs = [];
+      profile.chats.forEach(chatID =>
+        chatIDs.push(
+          props.firebase
+            .chats()
+            .where('chatID', '==', chatID)
+            .get()
+        )
+      );
+      Promise.all(chatIDs).then(results => {
+        const mappedResult = results.map(result =>
+          result.docs.map(doc => doc.data())
+        );
+        console.log(mappedResult);
+        const groupByFromUser = groupBy('fromUser');
+        const groupedMessages = groupByFromUser(mappedResult.flat());
+        console.log(Object.entries(groupedMessages));
 
-    return (
-        <div>
-            chat page, with a list of chats - date of last message and from what user. click to go to list of messages
-      {state.length > 0 && state.sort((item1, item2) => item1.timestamp > item2.timestamp).map((message,index) => 
-          {            
-           return (<div key={index}>
-               {message.fromUser},{message.message}, {new Date(message.timestamp).toLocaleDateString('en-GB', { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })}
-            </div>)
-            })
-        } 
-        </div>
-    )
-}
+        setState(Object.entries(groupedMessages));
+      });
+    }
+
+    // const messages$ = combineLatest(fromRef$, toRef$).pipe(
+    //     flatMap(msgs => {
+    //         const [incoming, sent] = msgs;
+    //         return [...incoming, ...sent]
+    //     })
+    // )
+    // messages$.subscribe((item) => {
+    //     const messages = [...state, item];
+    //     setState(messages)
+    // })
+    console.log(state);
+  }, [profile.id]);
+
+  return (
+    <div>
+      chat page, with a list of chats - date of last message and from what user.
+      click to go to list of messages
+      {state.length > 0 &&
+        state
+          // .sort((item1, item2) => item1.timestamp > item2.timestamp)
+          .map((message, index) => {
+            return (
+              <div key={index}>
+                {message[0]}
+                <MessagesList messages={message[1]}></MessagesList>
+                {/* {new Date(message.timestamp).toLocaleDateString('en-GB', {
+                  month: 'long',
+                  day: 'numeric',
+                  hour: '2-digit',
+                  minute: '2-digit'
+                })} */}
+              </div>
+            );
+          })}
+    </div>
+  );
+};
 
 const ChatsComponent = withFirebase(ChatsBase);
-export default ChatsPage; 
+export default ChatsPage;
